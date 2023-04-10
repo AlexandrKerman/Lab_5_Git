f5f7c40106857b92a250dccddbb5fed89c3eff9c
Alexandr <aakolomeycev@gmail.com>
2023-04-10 19:14:44 +0300

    1. Создан проект. 2. Добавлена функция для вычисления варианта.

---
+++ main.py
@@ -0,0 +1,16 @@
+# Вычисление варианта (мне лень самому считать)
+
+def variant ():
+    Student = {
+        "Group": 13,
+        "Num": 7
+    }
+    N = ((Student["Group"]-1)*10+100+Student["Num"])%187
+    print (N)
+
+def main ():
+    pass
+
+if __name__ == "__main__":
+    variant()
+    main ()
